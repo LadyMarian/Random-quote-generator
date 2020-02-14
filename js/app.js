cdd34f8b4a1eeb685e297d4ae9edf77ebb4aaf9b
LadyMarian <45603798+LadyMarian@users.noreply.github.com>
2020-02-14 11:57:40 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,43 @@
+const quotes = [
+    {
+      quote:
+        "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
+      author: "Life"
+    },
+    {
+      quote:
+        "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
+      author: "John Kenneth Galbraith"
+    },
+    {
+      quote:
+        "God save me from my friends. I can protect myself from my enemies.",
+      author: "Claude Louis Hector de Villars "
+    },
+    {
+      quote: "The price of anything is the amount of life you exchange for it.",
+      author: "David Thoreau"
+    },
+    {
+      quote:
+        "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
+      author: "Charles Lindbergh"
+    },
+    {
+      quote:
+        "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
+      author: "Tyne Daly"
+    }
+  ];
+
+const button = document.getElementById("generate-btn");
+const newQuote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+function clickButton() {
+	const pickRandom = quotes[Math.floor(Math.random() * (quotes.length - 1))];
+	newQuote.innerHTML = pickRandom.quote;
+	author.innerHTML = pickRandom.author;
+}
+
+button.addEventListener("click", clickButton);
